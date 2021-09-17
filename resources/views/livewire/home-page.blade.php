255ae866f88b724b7bf90dfa4c4b7bcb9bd8ec17
almasemilius <almasemilius@gmail.com>
2021-09-17 21:01:40 +0300

    spatie installation

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- Be like water. --}}
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem ex commodi nisi! Vel laboriosam facere modi. Laborum sequi, ullam quo, dolorem sed voluptatum officiis maxime doloribus asperiores fugiat a tenetur.
 </div>
