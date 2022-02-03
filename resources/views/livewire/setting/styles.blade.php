39a8fc34fa143c2a6a0324bac0f576ae418244af
Almasemilius <almasemilius@gmail.com>
2022-02-03 01:06:51 -0800

    Space between add image btn and add style fix styles edit

--- resources/views/livewire/setting/styles.blade.php
+++ resources/views/livewire/setting/styles.blade.php
@@ -43,26 +43,24 @@
                                     <textarea wire:model.defer="style.info" id="styleInfo" type="textarea" class="border-0 px-3 py-3 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"></textarea>
                                 </div>
                             </div>
-                            <div class="flex">
-                                <div class="lg:w-6/12 px-4">
-                                    <div class="relamb-3">
-                                        <label class="block capitalize text-xs font-bold mb-2" for="">
-                                            Image
-                                        </label>
-                                        <label class="w-64 flex flex-col items-center px-4 py-1 rounded-lg shadow-lg tracking-wide uppercase cursor-pointer bg-brownish">
-                                            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                            </svg>
-                                            <span class="mt-2 text-base leading-normal">Select a file</span>
-                                            <input wire:model="styleImage" type="file" class="hidden">
-                                        </label>
-                                    </div>
+                            <div class="lg:w-6/12 px-4">
+                                <div class="relamb-3">
+                                    <label class="block capitalize text-xs font-bold mb-2" for="">
+                                        Image
+                                    </label>
+                                    <label class="w-64 flex flex-col items-center px-4 py-1 rounded-lg shadow-lg tracking-wide uppercase cursor-pointer bg-brownish">
+                                        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                        </svg>
+                                        <span class="mt-2 text-base leading-normal">Select a file</span>
+                                        <input wire:model="styleImage" type="file" class="hidden">
+                                    </label>
                                 </div>
-                                <div class="lg:w-6/12 px-4">
-                                    <label for="">&nbsp</label>
-                                    <div class="relative mb-3  text-center">
-                                        <button wire:click.prevent="addStyle" class="w-64 shadow-lg bg-brownish rounded-md px-4 py-6">Add Style</button>
-                                    </div>
+                            </div>
+                            <div class="lg:w-6/12 px-4">
+                                <label for="">&nbsp</label>
+                                <div class="relative mb-3  text-center">
+                                    <button wire:click.prevent="addStyle" class="w-64 shadow-lg bg-brownish rounded-md px-4 py-6">Add Style</button>
                                 </div>
                             </div>
                         </div>
